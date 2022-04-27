85eb04c13cc948a7d30b3d39c647246c8ec41d9d
sorakann iMac <ski@student.42lausanne.com>
2022-04-27 11:12:26 +0200

    ebauche gestion signe $ avec dollar_translate()

--- srcs/temporaire.c
+++ srcs/temporaire.c
@@ -6,7 +6,7 @@
 /*   By: ski <ski@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/26 17:18:13 by ski               #+#    #+#             */
-/*   Updated: 2022/04/27 10:16:53 by ski              ###   ########.fr       */
+/*   Updated: 2022/04/27 11:09:01 by ski              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +28,23 @@ void write_exit_success(t_vars *vars)
 void write_exit_failure(t_vars *vars)
 {
 	update_var(&vars->loc, "?", EXIT_FAILURE_STR);
+}
+
+/* ************************************************************************** */
+char *dollar_translate(t_vars *vars, char *dollar_var)
+{
+	char *var_name;
+
+	// var_name = foreign_word;		// exclus le signe $
+	var_name = dollar_var + 1;	// inclus le signe $   
+		
+	if (does_var_exist(vars->env, var_name))
+		return (get_var(vars->env, var_name)->data);
+
+	if (does_var_exist(vars->loc, var_name))
+		return (get_var(vars->loc, var_name)->data);
+
+	return (NULL);
 }
 
 /* ************************************************************************** */
