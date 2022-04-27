c2b71cb43e4fd578ba7dbd7ef5cedfafe0b5818d
gudias <gudias@42.ch>
2022-04-27 20:26:44 +0200

    wip

--- srcs/parsing.c
+++ srcs/parsing.c
@@ -6,7 +6,7 @@
 /*   By: gudias <marvin@42lausanne.ch>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/25 16:34:50 by gudias            #+#    #+#             */
-/*   Updated: 2022/04/26 22:18:12 by gudias           ###   ########.fr       */
+/*   Updated: 2022/04/27 20:25:27 by gudias           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,10 +28,23 @@ void	parse_line(t_vars *vars, char *line)
 	if (!cmd_args[0])
 		return ;
 	add_history(line);
+	//if ("name=value" && pas de cmd) 
+	// !!! si cmd ->ignore
+	
 	if (is_builtin(cmd_args[0]))
 		exec_builtin(vars, cmd_args);
 	else
-		run_cmd(vars, line, 1);
+		run_cmd(vars, line, 1);  //modif line ---> cmd_args (changer parsing dans exec_cmd)
+
+	//update '$?' ici ?? (besoin de valeur de retour d'exec)
+	//pourquoi?
+	//$? est une variable du shell, elle se met a jour apres l'execution de qqch
+	//peu importe quoi
+	//un programme lancer ne doit pas gerer les var de notre shell mais retourne just 
+	//son exit status: exit(0); exit(1); return(-1); etc...
+	//il faut recuperer cette valeur peu importe ce qu'on a fait et mettre a jour apres
+	// (ca me semble plus coherent)
+	// !! modifier fonction pour qu'elle retourne le code erreur
 	
 	ft_free_array(cmd_args);
 }
