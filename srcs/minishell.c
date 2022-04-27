0ded4a1aaea221c928f7068faf31a97c8726bd65
sorakann iMac <ski@student.42lausanne.com>
2022-04-27 08:06:49 +0200

    rajout de: [vars->loc = NULL] dans init_vars()

--- srcs/minishell.c
+++ srcs/minishell.c
@@ -6,7 +6,7 @@
 /*   By: ski <ski@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/12 17:15:58 by gudias            #+#    #+#             */
-/*   Updated: 2022/04/27 07:49:01 by ski              ###   ########.fr       */
+/*   Updated: 2022/04/27 08:05:11 by ski              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,7 @@ static void	init_vars(t_vars *vars, char **envp)
 	vars->stdout_fd = dup(1);
 	vars->stderr_fd = dup(2);
 	vars->env = NULL;
+	vars->loc = NULL;
 	init_env(vars, envp);
 	init_loc(vars);
 }
