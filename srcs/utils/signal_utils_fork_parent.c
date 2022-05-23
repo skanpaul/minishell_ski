b29a04a9da5c52a99d9ace6d1473f9f6c615799e
Sorakannn SurfacePro7 <sorakann.ki@gmail.com>
2022-05-23 19:43:57 +0200

    norminette: signal_utils_fork_parent.c

--- srcs/utils/signal_utils_fork_parent.c
+++ srcs/utils/signal_utils_fork_parent.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   signal_utils_fork_parent.c                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ski <ski@student.42.fr>                    +#+  +:+       +#+        */
+/*   By: sorakann <sorakann@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/15 09:09:38 by sorakann          #+#    #+#             */
-/*   Updated: 2022/05/19 17:53:07 by ski              ###   ########.fr       */
+/*   Updated: 2022/05/23 19:43:32 by sorakann         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,10 +26,8 @@ void	init_signal_fork_parent(t_sig *s)
 /* ************************************************************************** */
 static void	init_struct_sa_fork_parent(t_sig *s)
 {
-	// SIGINT -----------------------------------------	
 	s->sa_sigint.sa_handler = &handler_signal_fork_parent;
 	s->sa_sigint.sa_flags = SA_RESTART;
-	// SIGQUIT -----------------------------------------	
 	s->sa_sigquit.sa_handler = SIG_IGN;
 }
 
