d4690ff2ada1381a75585bc8cdb00926818583c1
sorakann iMac <ski@student.42lausanne.com>
2022-05-23 16:29:47 +0200

    check_grammar_pipeline.c

--- srcs/utils/check_grammar_pipeline.c
+++ srcs/utils/check_grammar_pipeline.c
@@ -6,7 +6,7 @@
 /*   By: ski <ski@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/12 12:09:45 by sorakann          #+#    #+#             */
-/*   Updated: 2022/05/17 11:12:29 by ski              ###   ########.fr       */
+/*   Updated: 2022/05/23 16:29:30 by ski              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,49 +14,43 @@
 
 /* ************************************************************************** */
 static bool	is_token_pipeline_correct(char **array, t_vars *vars);
-static bool is_previous_token_correct(char **array, t_vars *vars);
+static bool	is_previous_token_correct(char **array, t_vars *vars);
 static bool	is_next_token_correct(char **array, t_vars *vars);
 
 /* ************************************************************************** */
 // return: FALSE if grammar is not correct and 
 bool	is_grammar_pipeline_correct(char *line, t_vars *vars)
 {
-    char	**array;
+	char	**array;
 
 	if (!line)
 		return (false);
-	
 	array = NULL;
 	array = split_shell_line(line, ' ');
-	// print_array_in_line(array, "A)   tokens     :\t ");
 	translate_dollars_all(array, vars);
-	// print_array_in_line(array, "B)   tok. trans.:\t ");
-
-    if (!is_token_pipeline_correct(array, vars)
+	if (!is_token_pipeline_correct(array, vars)
 		|| !is_previous_token_correct(array, vars)
 		|| !is_next_token_correct(array, vars))
 		return (false);
-    
-    free_array(array);
-    // free_array(&array);
-    return (true);
+	free_array (array);
+	return (true);
 }
 
 /* ************************************************************************** */
 // pipeline token like [ || , ||| , |||| , ... ] are NOT correct
 // -------------------------------------------------
-static bool is_token_pipeline_correct(char **array, t_vars *vars)
+static bool	is_token_pipeline_correct(char **array, t_vars *vars)
 {
-	int i;
+	int	i;
 
 	i = 0;
-	while(array[i])
+	while (array[i])
 	{
 		if (does_wordstart_match(array[i], "|"))
 		{
 			if (!does_word_match(array[i], "|"))
 			{
-				update_var(&vars->loc, "?", "258"); // à refléchir
+				update_var(&vars->loc, "?", "258");
 				ft_printf("minishell: syntax error near token ");
 				ft_printf("\'%s\'\n", array[i]);
 				return (false);
@@ -72,28 +66,27 @@ static bool is_token_pipeline_correct(char **array, t_vars *vars)
 //		a) token starting with [ < , << , <<< , <<<< , ... ]
 //		b) token starting with [ > , >> , >>> , >>>> , ... ]
 // -------------------------------------------------
-static bool is_previous_token_correct(char **array, t_vars *vars)
+static bool	is_previous_token_correct(char **array, t_vars *vars)
 {
-	int i;
+	int	i;
 
 	i = 0;
-	while(array[i])
+	while (array[i])
 	{
-		if(does_word_match(array[i], "|"))
+		if (does_word_match(array[i], "|"))
 		{
 			if (i == 0
 				|| does_wordstart_match(array[i - 1], "<")
 				|| does_wordstart_match(array[i - 1], ">"))
 			{
-				update_var(&vars->loc, "?", "258"); // à refléchir
+				update_var(&vars->loc, "?", "258");
 				ft_printf("minishell: syntax error near token ");
 				ft_printf("\'%s\'\n", array[i]);
 				return (false);
-			}			
+			}
 		}
 		i++;
 	}
-
 	return (true);
 }
 
@@ -101,27 +94,26 @@ static bool is_previous_token_correct(char **array, t_vars *vars)
 // pipeline token [ | ] can NOT be FOLLOWED by
 //		a) empty token
 // -------------------------------------------------
-static bool is_next_token_correct(char **array, t_vars *vars)
+static bool	is_next_token_correct(char **array, t_vars *vars)
 {
-	int i;
+	int	i;
 
 	i = 0;
-	while(array[i])
+	while (array[i])
 	{
-		if(does_word_match(array[i], "|"))
+		if (does_word_match(array[i], "|"))
 		{
 			if (array[i + 1] == NULL
 				|| array[i + 1][0] == '\0')
 			{
-				update_var(&vars->loc, "?", "258"); // à refléchir
+				update_var(&vars->loc, "?", "258");
 				ft_printf("minishell: syntax error near token ");
 				ft_printf("\'%s\'\n", array[i]);
 				return (false);
-			}			
+			}
 		}
 		i++;
 	}
-
 	return (true);
 }
 
@@ -131,23 +123,18 @@ static bool is_next_token_correct(char **array, t_vars *vars)
 // char	*check_grammar_pipeline(char *line, t_vars *vars)
 // {
 //     char	**array;
-
 // 	if (!line)
-// 		return (NULL);
-	
+// 		return (NULL);	
 // 	array = NULL;
 // 	array = split_shell_line(line, ' ');
 // 	// print_array_in_line(array, "A)   tokens     :\t ");
 // 	translate_dollars_all(array, vars);
 // 	// print_array_in_line(array, "B)   tok. trans.:\t ");
-
 //     if (!is_token_pipeline_correct(array, vars)
 // 		|| !is_previous_token_correct(array, vars)
 // 		|| !is_next_token_correct(array, vars))
-// 		ft_free_null((void **)&line);
-    
+// 		ft_free_null((void **)&line);    
 //     free_array(&array);
 //     return (line);
 // }
-
 /* ************************************************************************** */
