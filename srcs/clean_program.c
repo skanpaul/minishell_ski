9647015b210a4d013290385c9957074fb66b78a8
gudias <gudias@42.ch>
2022-05-25 18:48:38 +0200

    added rl_clean_history()

--- srcs/clean_program.c
+++ srcs/clean_program.c
@@ -6,7 +6,7 @@
 /*   By: ski <ski@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/05 09:23:17 by ski               #+#    #+#             */
-/*   Updated: 2022/05/23 15:51:25 by ski              ###   ########.fr       */
+/*   Updated: 2022/05/25 18:26:13 by gudias           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 /* ************************************************************************** */
 void	clean_program(t_vars *vars)
 {
+	rl_clear_history();
 	free_var_list(&vars->env);
 	free_var_list(&vars->loc);
 	ft_free_null((void **)&vars->new_line);
